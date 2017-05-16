bfaf7e6ff64326cbd896999a6fde986a5285aa35
Scott Wang <swang@Scotts-MacBook-Pro-2.local>
2017-05-15 21:33:06 -0700

    master no longer needs that. all tags pushed to branches

--- php7-xenial/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker build --pull -t revenuewire/php-app:7-xenial .
-docker push revenuewire/php-app:7-xenial
--- php7-xenial/httpd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source /etc/apache2/envvars
-apache2 -V
-/usr/sbin/apache2 -D FOREGROUND
